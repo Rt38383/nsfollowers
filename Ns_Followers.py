f93307ceb4ca097a40f1061b29563f38ca5fc502
Rt38383 <hsbshshwbssb@gmail.com>
2024-05-17 20:33:46 +0530

    Update Ns_Followers.py

--- Ns_Followers.py
+++ Ns_Followers.py
@@ -11,9 +11,9 @@ class Ns_Followers:
         self.requests=requests
         self.__AES_NoPadding = AES_CBC_NoPadding()
 
-        self.__userid='...' #Enter UserID Here
-        self.__username='...' #Enter UserName Here
-        self._session='...' #Enter SessionID Here
+        self.__userid='66378414251'
+        self.__username='rajesh866767'
+        self._session='66378414251%3AEQJpj6Om1S5kIe%3A21%3AAYfiA95jNfREje8SThnYHqsBQ_D7EzpfbsciR_HpmA'
 
         self.__Pk=self.__userid.encode()+b'*'+str(random.randint(111,999)).encode()
         self.__Ip=os.urandom(8).hex().encode()+b'*'+str(random.randint(111,999)).encode()
